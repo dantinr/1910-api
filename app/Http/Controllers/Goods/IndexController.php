967c0bf843dee7146431c1fbc9331cc7d16ee327
dante <dantinr@gmail.com>
2020-08-23 20:11:01 +0800

    商品接口

---
+++ app/Http/Controllers/Goods/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Goods;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * 获取商品接口
+     */
+    public function home()
+    {
+        $url = env("SHOP_HOST") . '/api/goods/index';
+        //$data = file_get_contents($url);        //获取商品数据
+
+        $data = [];
+        $response = [
+            'errno' => 0,
+            'msg'   => 'ok',
+            'data'  => [
+                'goods' => $data
+            ]
+        ];
+
+        return $response;
+    }
+
+}
